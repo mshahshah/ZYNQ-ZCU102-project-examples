02e0d830aaad99090b0358908bcaaf30c7db77c9
Masoud Shahshahani <masoud.s@samsung.com>
2022-10-14 14:56:17 -0500

    removed all extra files

--- cnn_models/demo_files/demo_case_2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-testcase=2
-echo "Target : Test Full synthesize on LeNet at 100 MHz on Minimal Pragma!"
-yes | cp -rf demo_files/input_arguments_$testcase.yaml input_arguments.yaml
-python dnn_framework.py -mode syn
--- src/run_dnn_hw_gen.sh
+++
@@ -1,2 +0,0 @@
-
-vivado -notrace -mode batch -source src/dnn_hw_gen_shared_mem.tcl -tclargs $1
